fb7fc986aafaf085c523edf21f14fcfd216a15f3
c1m50c <terminalityms@gmail.com>
2021-10-27 18:51:24 -0500

    Comments, Interval Input

--- src/main.py
+++ src/main.py
@@ -8,38 +8,51 @@ import numpy as np
 import random
 
 
-ARR_SIZE: int = 30
-INTERVAL_TIME: int = 100
-arr: list[int] = [  ]
+ARR_SIZE: int = 30 # Size of the visualized array.
+interval_time: int = 75 # Interval between frames.
+arr: list[int] = [  ] # Array to visualize being sorted.
 
 
 ALGORITHMS = {
+    # "algorithm_name": algorithm_function,
     "selection_sort": selection_sort,
     "insertion_sort": insertion_sort,
 }
 
 
 def main():
+    global interval_time
     print(f"{Style.BRIGHT}{Fore.BLUE}Visual Sorting Algorithms{Style.RESET_ALL}")
+    print(f"{Style.BRIGHT}{Fore.GREEN}Default Interval: {Fore.RESET}{interval_time}{Style.RESET_ALL}")
     print(f"{Style.BRIGHT}{Fore.GREEN}Valid Algorithms: {Style.RESET_ALL}(")
     for key in ALGORITHMS.keys():
         print(f". {Fore.CYAN}{key}{Fore.RESET}")
-    
     print(")\n")
-    inp: str = input(f"{Style.BRIGHT}{Fore.MAGENTA}algorithm{Fore.YELLOW}:${Style.RESET_ALL} ")
-    inp = inp.lower()
     
+    # Interval Input: Set `interval_time` to a user-defined value. #
+    interval_input: str = input(f"{Style.BRIGHT}{Fore.MAGENTA}interval{Fore.YELLOW}:${Style.RESET_ALL} ")
+    if interval_input.isdigit():
+        interval_time = int(interval_input)
+    else:
+        print(f"{Style.BRIGHT}{Fore.RED}ERROR: {Fore.RESET}Cannot convert interval input to integer, using default interval.{Style.RESET_ALL}")
     
+    # Algorithm Input: Get user-input for which algorithm to run. #
+    algorithm_input: str = input(f"{Style.BRIGHT}{Fore.MAGENTA}algorithm{Fore.YELLOW}:${Style.RESET_ALL} ")
+    algorithm_input = algorithm_input.lower()
+    
+    # Array Creation: Create array for visualization. #
     arr.clear()
     for i in range(ARR_SIZE):
         arr.append(random.randint(0, ARR_SIZE))
     
-    if inp in ALGORITHMS.keys():
-        ALGORITHMS[inp](arr)
+    # Algorithm Input Check: Check if the `algorithm_input` contains a valid algorithm, else quit. #
+    if algorithm_input in ALGORITHMS.keys():
+        ALGORITHMS[algorithm_input](arr)
     else:
         quit()
     
-    animation = camera.animate(interval=INTERVAL_TIME)
+    # Animate #
+    animation = camera.animate(interval=interval_time)
     plt.show()
 
 
