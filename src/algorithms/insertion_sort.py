a696c2d9838ca4e21d99f656a0e64cebb69fd22a
c1m50c <terminalityms@gmail.com>
2021-10-27 18:40:26 -0500

    InsertionSort, Algorithm Input

---
+++ src/algorithms/insertion_sort.py
@@ -0,0 +1,19 @@
+from animate_graph import plot
+
+def insertion_sort(arr: list[int]):
+    """
+    ## Complexities:
+    ```py
+    Worst Case Time Complexity == O(n * n)
+    Average Case Time Complexity == O(n * n)
+    Best Case Time Complexity == O(n)
+    Space Complexity == O(n) Total, O(1) Auxiliary
+    ```
+    """
+    
+    for i in range(1, len(arr)):
+        j = i
+        while j > 0 and arr[j] < arr[j - 1]:
+            arr[j - 1], arr[j] = arr[j], arr[j - 1]
+            j = j - 1
+            plot(j, arr)
--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from algorithms.selection_sort import selection_sort
+from algorithms.insertion_sort import insertion_sort
 
+from colorama import Style, Fore
 from animate_graph import camera
 import matplotlib.pyplot as plt
 import numpy as np
@@ -11,11 +13,32 @@ INTERVAL_TIME: int = 100
 arr: list[int] = [  ]
 
 
+ALGORITHMS = {
+    "selection_sort": selection_sort,
+    "insertion_sort": insertion_sort,
+}
+
+
 def main():
+    print(f"{Style.BRIGHT}{Fore.BLUE}Visual Sorting Algorithms{Style.RESET_ALL}")
+    print(f"{Style.BRIGHT}{Fore.GREEN}Valid Algorithms: {Style.RESET_ALL}(")
+    for key in ALGORITHMS.keys():
+        print(f". {Fore.CYAN}{key}{Fore.RESET}")
+    
+    print(")\n")
+    inp: str = input(f"{Style.BRIGHT}{Fore.MAGENTA}algorithm{Fore.YELLOW}:${Style.RESET_ALL} ")
+    inp = inp.lower()
+    
+    
     arr.clear()
     for i in range(ARR_SIZE):
         arr.append(random.randint(0, ARR_SIZE))
-    selection_sort(arr)
+    
+    if inp in ALGORITHMS.keys():
+        ALGORITHMS[inp](arr)
+    else:
+        quit()
+    
     animation = camera.animate(interval=INTERVAL_TIME)
     plt.show()
 
