806234ebaf7792121522e98dfee58ed763afb7d3
c1m50c <terminalityms@gmail.com>
2021-10-27 21:17:20 -0500

    animate graph docstring 💬

--- src/animate_graph.py
+++ src/animate_graph.py
@@ -9,7 +9,17 @@ camera = Camera(fig)
 comparisons = 0
 
 
-def plot(highlight, data, other_highlights: list[int] = [  ]):
+def plot(highlight: int, data: list, other_highlights: list[int] = [  ]):
+    """
+    To be used in animating a plot figure, see `algorithms` to see examples of its usage.
+    ## Parameters:
+    ```py
+    highlight: int # Index of value to highlight.
+    data: list # The array containing the values being sorted.
+    other_highlights: list[int] = [  ] # Other points important enough to be highlighted, Different color than normal highlight.
+    ```
+    """
+    
     arr = list(range(len(data)))
     global comparisons
     comparisons += 1
