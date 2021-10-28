00cd2950ea4d0568eca9d230afbf871228f0d057
c1m50c <terminalityms@gmail.com>
2021-10-28 15:54:40 -0500

    Implementing Tests 🧪

--- src/algorithms/selection_sort.py
+++ src/algorithms/selection_sort.py
@@ -18,3 +18,13 @@ def selection_sort(arr: list[int]):
                 cur_min_idx = j
                 plot(i, arr, other_highlights=[cur_min_idx])
         arr[i], arr[cur_min_idx] = arr[cur_min_idx], arr[i] # Swap
+
+
+def test_with_ints():
+    arr: list[int] = [ 4, 3, 2, 1, 5, 5, 0 ]
+    assert arr == arr.sort()
+
+
+def test_with_floats():
+    arr: list[float] = [ 1.3, 1.0, 5.55, 4.20, 9.76, 1.2 ]
+    assert arr == arr.sort()
