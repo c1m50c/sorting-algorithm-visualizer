edddd18559df75d315591dadc58c79c5e7e6f898
c1m50c <terminalityms@gmail.com>
2021-10-27 23:37:28 -0500

    Comb Sort

---
+++ src/algorithms/comb_sort.py
@@ -0,0 +1,32 @@
+from animate_graph import plot
+
+def next_gap(gap: int) -> int:
+    gap = (gap * 10) // 13
+    if gap < 1:
+        return 1
+    return gap
+
+
+def comb_sort(arr: list[int]):
+    """
+        ## Complexities:
+        ```py
+        Worst Case Time Complexity == O(n * n)
+        Average Case Time Complexity == O(n * n / 2^p) # p = number of increments
+        Best Case Time Complexity == O(n log n)
+        Space Complexity == O(1)
+        ```
+    """
+    
+    gap: int = len(arr)
+    swapped: bool = True
+ 
+    while gap != 1 or swapped == True:
+        gap = next_gap(gap)
+        swapped = False
+
+        for i in range(0, len(arr) - gap):
+            if arr[i] > arr[i + gap]:
+                arr[i], arr[i + gap] = arr[i + gap], arr[i]
+                swapped = True
+            plot(i, arr, other_highlights=[i + gap])
